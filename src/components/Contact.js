77b3d39c56dc756d31aef81a75b4c8f491d04fe5
Navya257 <navyasreeg025@gmail.com>
2023-10-14 20:59:25 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="mailto:navyasreeg025@gmai.com">navyasreeg025@gmai.com</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/navya-sree-2269ba237/" target="_blank" rel="noopener noreferrer">click here</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/Navya257" target="_blank" rel="noopener noreferrer">click here</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
+
